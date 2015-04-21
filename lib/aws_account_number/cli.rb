a21e1dea89bc1b483e1518fa5616a3489581fa65
ISOBE Kazuhiko <muramasa64@gmail.com>
2015-04-21 20:10:40 +0900

    support cloudformation stack

--- lib/aws_account_number/cli.rb
+++ lib/aws_account_number/cli.rb
@@ -7,12 +7,46 @@ module AwsAccountNumber
   class CLI < Thor
     include Thor::Aws
 
-    desc :get, "get AWS Account Number"
-    def get
+    default_command :security_group
+
+    desc :security_group, "get AWS Account Number from security group", alias: :s
+    def security_group
       begin
         puts ec2.client.describe_security_groups(group_names: ['default']).security_groups.first.owner_id
       rescue RuntimeError => e
         $stderr.puts e
+        exit 1
+      end
+    end
+
+    desc :cfn, "get AWS Account Number from CloudFormation stack", alias: :f
+    def cfn
+      template = <<-EOT
+      {
+        "Resources":{
+          "WaitHandle":{
+            "Type":"AWS::CloudFormation::WaitConditionHandle"
+          },
+          "Wait":{
+            "Type":"AWS::CloudFormation::WaitCondition",
+            "Properties":{
+              "Handle":{ "Ref":"WaitHandle" },
+              "Timeout":"1"
+            }
+          }
+        }
+      }
+      EOT
+      begin
+        result = cloudformation.client.create_stack(
+          stack_name: "accountnumber#{Time.now.to_i}",
+          template_body: template,
+          on_failure: "DELETE"
+        )
+        puts result.stack_id.split(':')[4]
+      rescue RuntimeError => e
+        $stderr.puts e
+        exit 1
       end
     end
   end
